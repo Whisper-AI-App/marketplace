272c95fd2bddb7c586da2a43334c010d34b70ac7
Matt Spence <admin@matt-spence.com>
2026-01-09 11:45:03 +0000

    mod: remove redundant model card

--- src/index.ts
+++ src/index.ts
@@ -99,23 +99,6 @@ export const whisperLLMCardsJson: WhisperLLMCardsJSON = {
 				},
 			},
 		},
-
-		"llama-3.2-1b-instruct-q4_0": {
-			name: "Whisper AI Chat (Ll 3.2 1B I Q4_0)",
-			type: "gguf",
-			sourceUrl:
-				"https://huggingface.co/unsloth/Llama-3.2-1B-Instruct-GGUF/resolve/main/Llama-3.2-1B-Instruct-Q4_0.gguf?q=4",
-			sizeGB: 0.72,
-			parametersB: 1,
-			ramGB: 1.5,
-			systemMessage: {
-				template:
-					"You are a 100% private on-device AI chat called Whisper. Conversations stay on the device. Help the user concisly. Be useful, creative, and accurate. Today's date is {date_time_string}.",
-				defaultTemplateValues: {
-					date_time_string: templateVariables.date_time_string.defaultValue,
-				},
-			},
-		},
 	},
 };
 
