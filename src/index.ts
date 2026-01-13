ebd7935712a79aaf94da084ad4fe417b0b2cf56a
Matt Spence <admin@matt-spence.com>
2026-01-13 13:54:33 +0000

    fix: clean up - confirmed that `granite-4.0-1b-GGUF` does not behave reliably on iOS with our current inference runtime

--- src/index.ts
+++ src/index.ts
@@ -83,23 +83,6 @@ export const whisperLLMCardsJson: WhisperLLMCardsJSON = {
 			},
 		},
 
-		// "granite-4.0-1b-GGUF": {
-		// 	name: "Whisper AI Chat (Grnt 4.0 1B Q4_K_M)",
-		// 	type: "gguf",
-		// 	sourceUrl:
-		// 		"https://huggingface.co/ibm-granite/granite-4.0-1b-GGUF/resolve/main/granite-4.0-1b-Q3_K_M.gguf",
-		// 	sizeGB: 0.86,
-		// 	parametersB: 1,
-		// 	ramGB: 1,
-		// 	systemMessage: {
-		// 		template:
-		// 			"You are a 100% private on-device AI chat called Whisper. Conversations stay on the device. Help the user concisly. Be useful, creative, and accurate. Today's date is {date_time_string}.",
-		// 		defaultTemplateValues: {
-		// 			date_time_string: templateVariables.date_time_string.defaultValue,
-		// 		},
-		// 	},
-		// },
-
 		"granite-4.0-h-micro-GGUF": {
 			name: "Whisper AI Chat (Grnt 4.0 H 3B Micro Q3_K_M)",
 			type: "gguf",
